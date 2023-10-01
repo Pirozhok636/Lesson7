5992709f88bd214ccd68f22cb21ec3d748c4fb76
Pirozhok <zhokpiro636@gmail.com>
2023-10-01 20:27:45 +0500

    Домашнее задание 7 урока

---
+++ app_advertisements/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Advertisement(models.Model):
+    class Meta:
+        db_table = 'advertisements'
+    title = models.CharField("Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("Торг", help_text="Уместен торг или нет")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'Advertisement(id={self.id}, title={self.title}, price={self.price})'
---
+++ app_lesson_4/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import less4
+
+urlpatterns = [
+    path('', less4)
+]
